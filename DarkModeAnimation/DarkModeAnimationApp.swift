d7209f915085449e4d99a0e200d4036651ce5bee
Justin Hold <36420615+jhold1982@users.noreply.github.com>
2023-10-04 14:39:01 -0500

    Initial Commit

---
+++ DarkModeAnimation/DarkModeAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  DarkModeAnimationApp.swift
+//  DarkModeAnimation
+//
+//  Created by Justin Hold on 10/4/23.
+//
+
+import SwiftUI
+
+@main
+struct DarkModeAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
