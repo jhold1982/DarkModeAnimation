1e082f5c0513189f6771967ab8bf3ef697066183
Justin Hold <36420615+jhold1982@users.noreply.github.com>
2023-10-05 11:32:43 -0500

    first commit

---
+++ DarkModeAnimation/RectKey.swift
@@ -0,0 +1,8 @@
+//
+//  RectKey.swift
+//  DarkModeAnimation
+//
+//  Created by Justin Hold on 10/4/23.
+//
+
+import Foundation
---
+++ DarkModeAnimation/ViewExtensions.swift
@@ -0,0 +1,18 @@
+//
+//  ViewExtensions.swift
+//  DarkModeAnimation
+//
+//  Created by Justin Hold on 10/4/23.
+//
+
+import SwiftUI
+
+struct ViewExtensions: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ViewExtensions()
+}
